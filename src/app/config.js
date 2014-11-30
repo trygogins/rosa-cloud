2318c28676ba9cc2b425575e86c89a8106f4666e
ovsyannikov <ovsyanng@gmail.com>
2014-11-30 21:16:00 +0400

    js new provider

--- src/app/config.js
+++ src/app/config.js
@@ -9,6 +9,11 @@
             "name" : "dropbox",
             "title" : "Dropbox",
             "url" : ""
+        },
+        {
+            "name" : "4shared",
+            "title" : "4Shared",
+            "url" : ""
         }
     ]
 }
